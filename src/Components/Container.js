14a02eb990baf3c0732d183ee2c52e7923e5321b
geccles <geccles01@gmail.com>
2019-04-21 13:36:27 -0400

    added images

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -2,15 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import color from '../styles/color';
 import Footer from './Footer';
+import WelcomeScreen from '../assets/Welcome_Screen.png'
+import LoginScreen from '../assets/Login_Screen.png'
 
 const BackgroundContainer = styled.div`
-	display: flex;
-	flex: 1;
 	top: 0;
 	width: 100%;
 	height: 100vh;
 	background: rgb(19,45,79);
 	background: linear-gradient(180deg, rgba(19,45,79,1) 27%, rgba(16,14,47,1) 100%);
+`
+const ImageContainer = styled.div`
+	display: flex;
+	img {
+		height: 60vh;
+		width: auto;
+	}
+`
+const TopNaviagtion = styled.div`
+	display: flex;
+	padding: 8px;
 `
 const Text = styled.p`
 	margin-block-start: 0;
@@ -18,39 +29,51 @@ const Text = styled.p`
 	color: ${color.white};
 `
 const TextContainerLeft = styled.div`
-	height: auto;
 	display: flex;
-	flex-direction: row;
-	flex: 1 1;
+	flex: 1;
 	justify-content: flex-start;
 `
 const TextContainerRight = styled.div`
 	display: flex;
-	flex-direction: row;
-	flex: 1 1;
+	flex: 1;
 	justify-content: flex-end;
 `
 const Link = styled.a`
 	padding: 16px;
     text-decoration: none;
 `
+const Wrapper = styled.div`
+	padding: 16px;
+`
 
 export default function PortfolioContainer () {
 	const links = [{ title: 'portfolio', link: '#' },
 	{ title: 'about', link: '#' },
 	{ title: 'contact', link: '#'}]
+
+	const images = [{ src: WelcomeScreen, alt: 'Welcome Screen' },
+	{ src: LoginScreen, alt: 'Login Screen' }]
 	return (
 		<BackgroundContainer>
-			<TextContainerLeft>
-				<Link href={'#'}><Text>Galina M. Nosti</Text></Link>
-			</TextContainerLeft>
-			<TextContainerRight>
+			<TopNaviagtion>
+				<TextContainerLeft>
+					<Link href={'#'}><Text>Galina Nosti</Text></Link>
+				</TextContainerLeft>
+				<TextContainerRight>
+					{
+						links.map(function (item, index){
+							return <Link key={index} href={item.link}><Text>{item.title}</Text></Link>
+						})
+					}
+				</TextContainerRight>
+			</TopNaviagtion>
+			<ImageContainer>
 				{
-					links.map(function (item, index){
-						return <Link key={index} href={item.link}><Text>{item.title}</Text></Link>
+					images.map(function (item, index){
+						return 	<Wrapper><img key={index} src={item.src} alt={item.alt} /></Wrapper>
 					})
 				}
-			</TextContainerRight>
+			</ImageContainer>
 			<Footer />
 		</BackgroundContainer>
 	)
